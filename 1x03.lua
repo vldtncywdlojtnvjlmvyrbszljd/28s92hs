12167644f3a713469b965c1649d66ff576d06f42
05650198416531d21d9 <160678881+vldtncywdlojtnvjlmvyrbszljd@users.noreply.github.com>
2024-08-25 11:44:10 +0700

    Update and rename 1x3.lua to 1x03.lua

--- 1x03.lua
+++ 1x03.lua
@@ -6,8 +6,8 @@ local screenGui = Instance.new("ScreenGui")
 screenGui.Parent = LocalPlayer:WaitForChild("PlayerGui")
 
 local frame = Instance.new("Frame")
-frame.Size = UDim2.new(0, 500, 0, 200)
-frame.Position = UDim2.new(0.2, -100, -0.5, -100)
+frame.Size = UDim2.new(0, 300, 0, 200)
+frame.Position = UDim2.new(0.5, -100, -0.5, -100)
 frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 frame.BorderSizePixel = 3
 frame.BorderColor3 = Color3.fromRGB(222, 142, 4)
