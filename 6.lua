0c83cb60771887b8345bef165228adc88bafbfbe
05650198416531d21d9 <160678881+vldtncywdlojtnvjlmvyrbszljd@users.noreply.github.com>
2024-08-25 13:04:27 +0700

    Update and rename 5.lua to 6.lua

--- 6.lua
+++ 6.lua
@@ -9,9 +9,10 @@ local frame = Instance.new("Frame")
 frame.Size = UDim2.new(0, 300, 0, 250)
 frame.Position = UDim2.new(0.2, -150, 0.5, -100)
 frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-frame.BorderSizePixel = 3
+frame.BorderSizePixel = 2
 frame.BorderColor3 = Color3.fromRGB(255, 255, 255)
 frame.Active = true
+frame.Shadow = true
 frame.Draggable = true
 frame.Parent = screenGui
 
