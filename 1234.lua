7161808ef01f89ad299e653a34443457b0a9d5e6
05650198416531d21d9 <160678881+vldtncywdlojtnvjlmvyrbszljd@users.noreply.github.com>
2024-08-25 12:59:36 +0700

    Update and rename 123.lua to 1234.lua

--- 1234.lua
+++ 1234.lua
@@ -87,8 +87,8 @@ checkKeyButton.BackgroundColor3 = Color3.fromRGB(0, 170, 0)
 checkKeyButton.Parent = frame
 
 local DiscordButton = Instance.new("TextButton")
-DiscordButton.Size = UDim2.new(0.4, 0, 0, 30)
-DiscordButton.Position = UDim2.new(0.1, 0, 0.4, 0)
+DiscordButton.Size = UDim2.new(0.8, 0, 0, 30)
+DiscordButton.Position = UDim2.new(0.10, 0, 0.850, 0)
 DiscordButton.Text = "Join Discord"
 DiscordButton.Font = Enum.Font.SourceSansBold
 DiscordButton.TextSize = 18
