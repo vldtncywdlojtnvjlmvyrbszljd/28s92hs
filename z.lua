bfae19a20aac286257fcee4e62aed5e079f211a1
05650198416531d21d9 <160678881+vldtncywdlojtnvjlmvyrbszljd@users.noreply.github.com>
2024-08-26 21:47:57 +0700

    Update and rename 1021.lua to z.lua

--- z.lua
+++ z.lua
@@ -1102,15 +1102,6 @@ elseif game.PlaceId == 7449423635 then
 end
 
 spawn(function() 
-    repeat
-        task.wait()
-    until game:IsLoaded()
-    repeat
-        task.wait()
-    until game.Players
-    repeat
-        task.wait()
-    until game.Players.LocalPlayer and game.Players.LocalPlayer.Team ~= nil 
     wait(1.5)
     require(game.ReplicatedStorage.Notification).new("<Color=Red>BRUTALITY HUB V4<Color=/>"):Display()
     wait(.5)
