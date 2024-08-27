de32b2f6b780f6011b64b9508705157fdfad520f
05650198416531d21d9 <160678881+vldtncywdlojtnvjlmvyrbszljd@users.noreply.github.com>
2024-08-27 14:01:52 +0700

    Update 13.lua

--- 13.lua
+++ 13.lua
@@ -3331,7 +3331,7 @@ print("Load Script")
 --Icon Tab
 local Library = Update:Window("                  PREMIUM","104397992902189",Enum.KeyCode.RightControl); --12523036534
 
-local H = Library:AddTab("Home","6026568198")
+local H = Library:AddTab("Status","104397992902189")
 local Main = Library:AddTab("Level Farm","104397992902189")
 local M = Library:AddTab("All Quest","104397992902189")
 local ESX = Library:AddTab("ESP & Sea","104397992902189")
