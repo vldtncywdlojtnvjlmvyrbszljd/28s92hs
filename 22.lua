2204d1a42bf2865116f62543a48472b58934ecf2
05650198416531d21d9 <160678881+vldtncywdlojtnvjlmvyrbszljd@users.noreply.github.com>
2024-08-28 21:16:55 +0700

    Update and rename 21.lua to 22.lua

---
+++ 22.lua
@@ -0,0 +1,5 @@
+local NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Notification-Library/Main/Library.lua"))()
+
+NotificationLibrary:SendNotification("Warning", "Script Loading ...", 10)
+NotificationLibrary:SendNotification("Warning", "YouTube: Medusa Script Roblox", 10)
+NotificationLibrary:SendNotification("Warning", "Made By: Medusa Script", 10)
