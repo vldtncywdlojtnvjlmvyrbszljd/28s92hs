51d1a72d789e7cfeba6d53799a77b4bcb467a480
05650198416531d21d9 <160678881+vldtncywdlojtnvjlmvyrbszljd@users.noreply.github.com>
2024-08-29 16:50:32 +0700

    Update and rename yk.lua to ykz.lua

---
+++ ykz.lua
@@ -0,0 +1,293 @@
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local TweenService = game:GetService("TweenService")
+
+local screenGui = Instance.new("ScreenGui")
+screenGui.Parent = LocalPlayer:WaitForChild("PlayerGui")
+
+local frame = Instance.new("Frame")
+frame.Size = UDim2.new(0, 300, 0, 250)
+frame.Position = UDim2.new(0.2, -150, 0.5, -100)
+frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+frame.BorderSizePixel = 2
+frame.BorderColor3 = Color3.fromRGB(255, 255, 255)
+frame.Active = true
+frame.Draggable = true
+frame.Parent = screenGui
+
+local headerLabel = Instance.new("TextLabel")
+headerLabel.Size = UDim2.new(1, 0, 0, 30)
+headerLabel.Position = UDim2.new(0, 0, 0, 0)
+headerLabel.Text = "BRUTALITY HUB V4"
+headerLabel.Font = Enum.Font.SourceSansBold
+headerLabel.TextSize = 18
+headerLabel.TextColor3 = Color3.fromRGB(222, 142, 4)
+headerLabel.BackgroundTransparency = 1
+headerLabel.TextWrapped = true
+headerLabel.Parent = frame
+
+local closeButton = Instance.new("TextButton")
+closeButton.Size = UDim2.new(0, 30, 0, 30)
+closeButton.Position = UDim2.new(1, -30, 0, 0)
+closeButton.Text = ""
+closeButton.Font = Enum.Font.SourceSansBold
+closeButton.TextSize = 18
+closeButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+closeButton.BackgroundTransparency = 1
+closeButton.Parent = frame
+
+closeButton.MouseButton1Click:Connect(function()
+    local tween = TweenService:Create(frame, TweenInfo.new(0.5), {Position = UDim2.new(0.5, -150, -0.5, -100)})
+    tween:Play()
+    tween.Completed:Connect(function()
+        screenGui:Destroy()
+    end)
+end)
+
+local label = Instance.new("TextLabel")
+label.Size = UDim2.new(1, 0, 0, 50)
+label.Position = UDim2.new(0, 0, 0, 12) 
+label.Text = "HELLO MY FRIEND "
+label.Font = Enum.Font.SourceSansBold
+label.TextSize = 30
+label.TextColor3 = Color3.fromRGB(255, 255, 255)
+label.BackgroundTransparency = 1
+label.TextWrapped = true
+label.Parent = frame
+
+local label = Instance.new("TextLabel")--baru ditambah
+label.Size = UDim2.new(1, 0, 0, 50)
+label.Position = UDim2.new(0, 0, 0, 35) 
+label.Text = "".. game.Players.LocalPlayer.Name
+label.Font = Enum.Font.SourceSansBold
+label.TextSize = 20
+label.TextColor3 = Color3.fromRGB(255, 255, 255)
+label.BackgroundTransparency = 1
+label.TextWrapped = true
+label.Parent = frame--sampai sini
+
+local label = Instance.new("TextLabel")--baru ditambah
+label.Size = UDim2.new(1, 0, 0, 50)
+label.Position = UDim2.new(0, 0, 0, 55) 
+label.Text = "".. identifyexecutor()
+label.Font = Enum.Font.SourceSansBold
+label.TextSize = 20
+label.TextColor3 = Color3.fromRGB(255, 255, 255)
+label.BackgroundTransparency = 1
+label.TextWrapped = true
+label.Parent = frame--sampai sini
+
+local textBox = Instance.new("TextBox")
+textBox.Size = UDim2.new(0.8, 0, 0, 30)
+textBox.Position = UDim2.new(0.1, 0, 0.4, 0)
+textBox.Text = "Enter Key Here"
+textBox.Font = Enum.Font.SourceSans
+textBox.TextSize = 18
+textBox.TextColor3 = Color3.fromRGB(0, 0, 0)
+textBox.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+textBox.TextWrapped = true
+textBox.Parent = frame
+
+local getKeyButton = Instance.new("TextButton")
+getKeyButton.Size = UDim2.new(0.4, 0, 0, 30)
+getKeyButton.Position = UDim2.new(0.1, 0, 0.7, 0)
+getKeyButton.Text = "Get Key"
+getKeyButton.Font = Enum.Font.SourceSansBold
+getKeyButton.TextSize = 18
+getKeyButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+getKeyButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0) --0, 170, 0
+getKeyButton.Parent = frame
+
+local checkKeyButton = Instance.new("TextButton")
+checkKeyButton.Size = UDim2.new(0.4, 0, 0, 30)
+checkKeyButton.Position = UDim2.new(0.5, 0, 0.7, 0)
+checkKeyButton.Text = "Check Key"
+checkKeyButton.Font = Enum.Font.SourceSansBold
+checkKeyButton.TextSize = 18
+checkKeyButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+checkKeyButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0) --0, 170, 0
+checkKeyButton.Parent = frame
+
+local DiscordButton = Instance.new("TextButton")
+DiscordButton.Size = UDim2.new(0.8, 0, 0, 30)
+DiscordButton.Position = UDim2.new(0.10, 0, 0.850, 0)
+DiscordButton.Text = "Join Discord"
+DiscordButton.Font = Enum.Font.SourceSansBold
+DiscordButton.TextSize = 18
+DiscordButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+DiscordButton.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+DiscordButton.Parent = frame
+
+local validationLabel = Instance.new("TextLabel")
+validationLabel.Size = UDim2.new(0.8, 0, 0, 30)
+validationLabel.Position = UDim2.new(0.1, 0, 0.550, 0) --0.1, 0, 0.850, 0
+validationLabel.Text = "Please Get Key"
+validationLabel.Font = Enum.Font.SourceSansBold
+validationLabel.TextSize = 18
+validationLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+validationLabel.BackgroundTransparency = 1
+validationLabel.Parent = frame
+
+local accountId = 44126; -- Plato account id [IMPORTANT]
+local allowPassThrough = false; -- Allow user through if error occurs, may reduce security
+local allowKeyRedeeming = true; -- Automatically check keys to redeem if valid
+local useDataModel = false;
+
+-- Plato callbacks
+local onMessage = function(message)
+    --logic
+end;
+
+-- Plato internals [START]
+local fRequest, fStringFormat, fSpawn, fWait = request or http.request or http_request or syn.request, string.format, task.spawn, task.wait;
+local localPlayerId = game:GetService("Players").LocalPlayer.UserId;
+local rateLimit, rateLimitCountdown, errorWait = false, 0, false;
+-- Plato internals [END]
+
+-- Plato global functions [START]
+function getLink()
+    return fStringFormat("https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/28s92hs/main/key.txt;
+end;
+
+function verify(key)
+    if errorWait or rateLimit then 
+        return false;
+    end;
+
+    onMessage("Checking key...");
+
+    if (useDataModel) then
+        local status, result = pcall(function() 
+            return game:HttpGetAsync(fStringFormat("https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/28s92hs/main/key.txt", key));
+        end);
+        
+        if status then
+            if string.find(result, "true") then
+                onMessage("Successfully whitelisted!");
+                return true;
+            elseif string.find(result, "false") then
+                --if allowKeyRedeeming then
+                if true then
+                    local status1, result1 = pcall(function()
+                        return game:HttpPostAsync(fStringFormat("https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/28s92hs/main/key.txt", key), {});
+                    end);
+
+                    if status1 then
+                        if string.find(result1, "true") then
+                            onMessage("Successfully redeemed key!");
+                            return true;
+                        end;
+                    end;
+                end;
+                
+                onMessage("Key is invalid!");
+                return false;
+            else
+                return false;
+            end;
+        else
+            onMessage("An error occured while contacting the server!");
+            return allowPassThrough;
+        end;
+    else
+        local status, result = pcall(function() 
+            return fRequest({
+                Url = fStringFormat("https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/28s92hs/main/key.txt", key),
+                Method = "GET"
+            });
+        end);
+
+        if status then
+            if result.StatusCode == 200 then
+                if string.find(result.Body, "true") then
+                    onMessage("Successfully whitelisted key!");
+                    return true;
+                else
+                    if (true) then --allowKeyRedeeming
+                        local status1, result1 = pcall(function() 
+                            return fRequest({
+                                Url = fStringFormat("https://api-gateway.platoboost.com/v1/authenticators/redeem/%i/%i/%s", accountId, localPlayerId, key),
+                                Method = "POST"
+                            });
+                        end);
+
+                        if status1 then
+                            if result1.StatusCode == 200 then
+                                if string.find(result1.Body, "true") then
+                                    onMessage("Successfully redeemed key!");
+                                    return true;
+                                end;
+                            end;
+                        end;
+                    end;
+                    
+                    return false;
+                end;
+            elseif result.StatusCode == 204 then
+                onMessage("Account wasn't found, check accountId");
+                return false;
+            elseif result.StatusCode == 429 then
+                if not rateLimit then 
+                    rateLimit = true;
+                    rateLimitCountdown = 10;
+                    fSpawn(function() 
+                        while rateLimit do
+                            onMessage(fStringFormat("You are being rate-limited, please slow down. Try again in %i second(s).", rateLimitCountdown));
+                            fWait(1);
+                            rateLimitCountdown = rateLimitCountdown - 1;
+                            if rateLimitCountdown < 0 then
+                                rateLimit = false;
+                                rateLimitCountdown = 0;
+                                onMessage("Rate limit is over, please try again.");
+                            end;
+                        end;
+                    end); 
+                end;
+            else
+                return allowPassThrough;
+            end;    
+        else
+            return allowPassThrough;
+        end;
+    end;
+end;
+
+getKeyButton.MouseButton1Click:Connect(function()
+    setclipboard(getLink())
+    validationLabel.Text = "Link Key Copied!"
+    validationLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+end)
+
+DiscordButton.MouseButton1Click:Connect(function()
+    setclipboard('https://discord.com/invite/brutality-hub-1182005198206545941')
+    validationLabel.Text = "Link Discord Copied!"
+    validationLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+end)
+
+checkKeyButton.MouseButton1Click:Connect(function()
+    local key = textBox.Text
+    if verify(key) then
+        validationLabel.Text = "Key Is Valid!"
+        validationLabel.TextColor3 = Color3.fromRGB(0, 255, 0)
+        wait(2)
+        validationLabel.Text = "Thanks For Use"
+        validationLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+        wait(2)
+        local tween = TweenService:Create(frame, TweenInfo.new(0.5), {Position = UDim2.new(0.5, -150, 1.5, -100)})
+        tween:Play()
+        tween.Completed:Connect(function()
+            screenGui:Destroy()
+        end)
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/asedesa/main/zxcv.lua",true))()
+    else
+        validationLabel.Text = "Checking Key..."
+        validationLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+        wait(1.7)
+        validationLabel.Text = "Key Is Not Valid!"
+        validationLabel.TextColor3 = Color3.fromRGB(255, 0, 0)
+    end
+end)
+
+wait(3)
+local tween = TweenService:Create(frame, TweenInfo.new(0.5), {Position = UDim2.new(0.5, -150, 0.5, -100)})
+tween:Play()
