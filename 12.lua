8de1387ce0e78e481f2f92177bef300bf2d455ad
05650198416531d21d9 <160678881+vldtncywdlojtnvjlmvyrbszljd@users.noreply.github.com>
2024-08-31 00:37:14 +0700

    Update 12.lua

--- 12.lua
+++ 12.lua
@@ -3900,7 +3900,7 @@ local D = Library:AddTab("Devil Fruit","18477363100")
 local Mh = Library:AddTab("Mod Hack","18797339934")
 local Misc = Library:AddTab("MISC","18477908150")
 --- Nama toggle ui
-NguyenTien:AddLabel("🚨 Alert : Mohon bersabar script Brutality Hub V4\n sedang dalam tahap perbaikan sementara fitur diminimalis\n gunakan terus script Brutality Hub V4 dengan developer asli orang KARAWANG")
+NguyenTien:AddLabel("🚨 Alert : Mohon bersabar script Brutality Hub V4\n sedang dalam tahap perbaikan sementara fitur diminimalis\n gunakan terus script Brutality Hub V4 dengan developer asli orang JAWA BARAT")
 
 NguyenTien:AddSeperator("Join Community")
 NguyenTien:AddButton("Discord Server",function()
