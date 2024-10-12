88f2d25b9ce555793264e8e0b4a5da5a956582e6
05650198416531d21d9 <160678881+vldtncywdlojtnvjlmvyrbszljd@users.noreply.github.com>
2024-10-12 13:06:21 +0700

    Update and rename ntf.lua to 11.lua

--- ntf.lua
+++
@@ -1,9 +0,0 @@
-local function pilihTimSecaraAcak()
-    local teams = {"Pirates", "Marines"} 
-    local randomIndex = math.random(1, #teams)
-    return teams[randomIndex]
-end
-
-game.Players.PlayerAdded:Connect(function(player)
-    player.Team = game.Teams[pilihTimSecaraAcak()]
-end)
