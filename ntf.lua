5b6c843daf3596538f18675dfbe1d02d4e2788ff
05650198416531d21d9 <160678881+vldtncywdlojtnvjlmvyrbszljd@users.noreply.github.com>
2024-10-12 13:02:55 +0700

    Update ntf.lua

--- ntf.lua
+++ ntf.lua
@@ -1,1 +1,9 @@
+local function pilihTimSecaraAcak()
+    local teams = {"Pirates", "Marines"} 
+    local randomIndex = math.random(1, #teams)
+    return teams[randomIndex]
+end
 
+game.Players.PlayerAdded:Connect(function(player)
+    player.Team = game.Teams[pilihTimSecaraAcak()]
+end)
